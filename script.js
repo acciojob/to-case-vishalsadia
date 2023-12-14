6bd5d02437b82d86a34979911880c6c037ceecf6
vishalsadia <125430740+vishalsadia@users.noreply.github.com>
2023-12-14 16:19:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function toCase(text) {
   // write your code here
+	 if (!text) {
+    return '-';
+  }
+	const lowercase = text.toLowerCase();
+  const uppercase = text.toUpperCase();
+  const result = `${lowercase}-${uppercase}`;
+  
+  return result;
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
-alert(toCase(text));
+// const text = prompt("Enter text:");
+// alert(toCase(text));
